7269684ef8b59bba3c83ef13ed5f49a19dd3606f
Ionică Bizău <bizauionica@gmail.com>
2017-10-11 14:02:03 +0300

    Working on the new version.

--- example/index.js
+++ example/index.js
@@ -1,8 +1,8 @@
-// Dependencies
-var PI = require("../lib");
+const PI = require("../lib");
 
+// Get first three decimals
 console.log(PI(3));
-// => 3.14
+// => 3.141
 
 console.log("First 16 decimals: " + PI(16, false));
 // => First 16 decimals: 141592653589793
